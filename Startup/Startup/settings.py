4dc816846233ffce568c0fe2e6220ba01064a311
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-06-14 17:00:02 +0300

    new commit

--- Startup/Startup/settings.py
+++ Startup/Startup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# Read SECRET_KEY from an environment variable
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-g7))jhrn-j8iihb0()d^=l3u&o*6-iwq4ccva)x!=yzmlm53ma')
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g7))jhrn-j8iihb0()d^=l3u&o*6-iwq4ccva)x!=yzmlm53ma'
+# SECRET_KEY = 'django-insecure-g7))jhrn-j8iihb0()d^=l3u&o*6-iwq4ccva)x!=yzmlm53ma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['backend-capstone-startup-01.herokuapp.com', '127.0.0.1']
 
@@ -132,6 +139,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -139,3 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
 }
+# Heroku: Update database configuration from $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
